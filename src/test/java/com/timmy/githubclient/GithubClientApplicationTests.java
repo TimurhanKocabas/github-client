679a9f7e13fcc78864965bc8204ebe99719ec115
timurhan kocabas <timurhankocabas@outlook.com>
2023-09-14 10:31:55 +0200

    start.spring.io java8, maven, springboot 2.7.15 project init

---
+++ src/test/java/com/timmy/githubclient/GithubClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timmy.githubclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
