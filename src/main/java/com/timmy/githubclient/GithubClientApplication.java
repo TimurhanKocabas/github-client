bc142ce4ab6f99cb9f1f6feb5e79189e46943ccd
timurhan kocabas <timurhankocabas@outlook.com>
2023-10-01 23:43:34 +0200

    adds: token injection to httprequest, environmental secrets, actuator for app details.

--- src/main/java/com/timmy/githubclient/GithubClientApplication.java
+++ src/main/java/com/timmy/githubclient/GithubClientApplication.java
@@ -2,8 +2,10 @@ package com.timmy.githubclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GithubProperties.class)
 public class GithubClientApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/timmy/githubclient/GithubProperties.java
@@ -0,0 +1,24 @@
+package com.timmy.githubclient;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Pattern;
+
+@ConfigurationProperties("github")
+@Validated
+public class GithubProperties {
+    /**
+     * Github API token ("user:sampleToken")
+     */
+    @Pattern(regexp ="\\w+" )
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
