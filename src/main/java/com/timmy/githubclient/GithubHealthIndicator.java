81d13891a66943653715c0216d1b11e9f1bad75d
timurhan kocabas <timurhankocabas@outlook.com>
2023-10-04 13:48:52 +0200

    adds: a health indicator implementation for github project we want to check events

---
+++ src/main/java/com/timmy/githubclient/GithubHealthIndicator.java
@@ -0,0 +1,33 @@
+package com.timmy.githubclient;
+
+import com.timmy.githubclient.github.GithubClient;
+import com.timmy.githubclient.github.RepositoryEvent;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubHealthIndicator implements HealthIndicator {
+
+    private GithubClient   githubClient;
+    public GithubHealthIndicator(GithubClient githubClient){
+        this.githubClient = githubClient;
+    }
+
+    @Override
+    public Health health() {
+        try{
+            ResponseEntity<RepositoryEvent[]> response=  this.githubClient.fetchEvents("spring-projects", "spring-boot");
+            if(response.getStatusCode().is2xxSuccessful()){
+                return Health.up().build();
+            }else{
+                return Health.down().build();
+            }
+        }catch (Exception exception){
+            return Health.down(exception).build();
+        }
+
+
+    }
+}
